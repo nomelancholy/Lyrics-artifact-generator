dbd0dda5c9b3d09a0ccf0f4a3334cc8ac0196f57
TakeKnowledge <starmekey@naver.com>
2022-04-07 17:26:11 +0900

    [U] 로컬 스토리지 활용해 최근 사용한 색 10가지 표시 기능 추가

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -4,16 +4,23 @@ export default function ColorPicker({
   $app,
   gradients,
   selectedGradient,
+  recentlyUsedGradients,
   onSelectBgColor,
 }) {
   this.state = {
     gradients,
-    recentlyUsedGradients: [],
+    recentlyUsedGradients,
     selectedGradient,
   };
 
+  this.setState = (nextState) => {
+    this.state = nextState;
+    console.log("this.state :>> ", this.state);
+    this.render();
+  };
+
   const $body = document.querySelector("body");
-  new Modal({ $app: $body, gradients, onSelectBgColor });
+  const modal = new Modal({ $app: $body, gradients, onSelectBgColor });
 
   this.$bgSelectBtn = document.createElement("button");
   this.$bgSelectBtn.innerHTML = "배경색 선택";
@@ -36,11 +43,41 @@ export default function ColorPicker({
     }
   };
 
-  $app.appendChild(this.$bgSelectBtn);
+  this.$recentlyColorPicker = document.createElement("div");
+  this.$recentlyColorPicker.className = "recently_color_picker";
 
-  const getRecentlyUsedGradients = () => {
-    console.log("최근 사용 색들");
-    // TO-DO : local storage에서 가져오기
+  this.render = () => {
+    console.log(
+      "this.state.recentlyUsedGradients :>> ",
+      this.state.recentlyUsedGradients
+    );
+
+    if (this.state.recentlyUsedGradients.length > 0) {
+      console.log(
+        "this.state.recentlyUsedGradients :>> ",
+        this.state.recentlyUsedGradients
+      );
+      this.$recentlyColorPicker.innerHTML = `
+      <ul>
+        ${this.state.recentlyUsedGradients
+          .map(
+            (gradient) =>
+              `<li style="background : linear-gradient(to bottom right, ${gradient})"></li>`
+          )
+          .join("")}
+      </ul>
+      `;
+    } else {
+      console.log("this.$recentlyColorPicker :>> ", this.$recentlyColorPicker);
+      this.$recentlyColorPicker.innerHTML =
+        "<p>최근 선택한 색깔이 없습니다</p>";
+    }
+
+    if (this.state.gradients) {
+      modal.setState({ ...modal.state, gradients: this.state.gradients });
+    }
   };
-  getRecentlyUsedGradients();
+
+  $app.appendChild(this.$bgSelectBtn);
+  $app.appendChild(this.$recentlyColorPicker);
 }
--- src/Preview.js
+++ src/Preview.js
@@ -7,6 +7,7 @@ export default function Preview({ $app, text, fontSize, fontColor }) {
     fontSize,
     fontColor,
     gradients: [],
+    recentlyUsedGradients: [],
     selectedGradient: "",
   };
 
@@ -22,45 +23,74 @@ export default function Preview({ $app, text, fontSize, fontColor }) {
     fontColor: this.state.fontColor,
   });
 
+  const colorPicker = new ColorPicker({
+    $app: this.$perviewSection,
+    gradients: this.state.gradients,
+    selectedGradient: this.state.selectedGradient,
+    onSelectBgColor: (e) => {
+      const $li = e.target.closest("li");
+
+      if ($li) {
+        const { colorName, colors } = $li.dataset;
+
+        let recentlyUsed = [];
+
+        if (localStorage.length > 0) {
+          recentlyUsed = JSON.parse(localStorage.getItem("recentlyUsed"));
+        }
+
+        // 최근 10번이내 선택하지 않은 경우
+        if (recentlyUsed.indexOf(colors) == -1) {
+          recentlyUsed.push(colors);
+
+          // 두 줄로 10개만 보여주자
+          if (recentlyUsed.length > 10) {
+            recentlyUsed.splice(0, recentlyUsed.length - 10);
+          }
+
+          localStorage.setItem("recentlyUsed", JSON.stringify(recentlyUsed));
+        }
+        this.setState({
+          selectedGradient: colors,
+          recentlyUsedGradients: recentlyUsed,
+        });
+      }
+    },
+  });
+
   this.setState = (nextState) => {
     this.state = nextState;
     canvas.setState({
       ...canvas.state,
-      ...nextState,
+      ...this.state,
+    });
+    colorPicker.setState({
+      ...colorPicker.state,
+      gradients: this.state.gradients,
+      recentlyUsedGradients: this.state.recentlyUsedGradients,
     });
   };
 
   const init = async () => {
+    console.log("getGradients");
     const response = await fetch("/gradients.json");
 
     if (response.ok) {
       const gradients = await response.json();
 
+      // 기본 색상 랜덤 선택
       const basicGraidentIndex = Math.floor(
         Math.random() * (gradients.length + 1)
       );
+
+      // 최근 선택
+      const recentlyUsed = JSON.parse(localStorage.getItem("recentlyUsed"));
 
       this.setState({
         ...this.state,
         gradients: gradients,
         selectedGradient: gradients[basicGraidentIndex].colors.join(","),
-      });
-
-      new ColorPicker({
-        $app: this.$perviewSection,
-        gradients: this.state.gradients,
-        selectedGradient: this.state.selectedGradient,
-        onSelectBgColor: (e) => {
-          const $li = e.target.closest("li");
-
-          if ($li) {
-            const { colorName, colors } = $li.dataset;
-
-            this.setState({
-              selectedGradient: colors,
-            });
-          }
-        },
+        recentlyUsedGradients: recentlyUsed,
       });
     }
   };
